29c8e27fb0dcce2b0255f40ab293227d172f5cea
Muhamad Rais Bin Abd Halim <mrais.abdhalim@gmail.com>
2012-11-19 11:32:04 +0800

    New file for create pdf from view file

---
+++ views/pdf-report-final.php
@@ -0,0 +1,8 @@
+<html>
+<head><title>This is pdf report</title></head>
+<body>
+	<h1>HELLO WORLD!</h1>
+	<hr>
+	<strong>Pdf report from view file</strong>
+</body>
+</html>
--- views/pdf-report.php
+++ views/pdf-report.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head><title>pdf report</title></head>
 	<body>
-	<h3>Create report : <a href="pdf_report/report">Here</a></h3>
+	<h3>Create report (from controller) : <a href="pdf_report/report">Here</a></h3>
+	<h3>Create report (from view file) : <a href="pdf_report/report_view">Here</a></h3>
 	</body>
 </html>
