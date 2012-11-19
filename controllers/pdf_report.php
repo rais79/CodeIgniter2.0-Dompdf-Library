00df10f69acf6a683cd57ffe97ba0079aca0af28
Muhamad Rais Bin Abd Halim <mrais.abdhalim@gmail.com>
2012-11-19 11:41:30 +0800

    Add method for create pdf using view file

--- controllers/pdf_report.php
+++ controllers/pdf_report.php
@@ -13,7 +13,7 @@ class Pdf_report extends CI_Controller {
 				<body>
 					<h1>HELLO WORLD!</h1>
 					<hr>
-					<strong>I\'m pdf report</strong>
+					<strong>Pdf report from controller</strong>
 				</body>
 				</html>
 				';
@@ -21,6 +21,14 @@ class Pdf_report extends CI_Controller {
 		$pdf_filename  = 'My-report.pdf';
 		$this->load->library('dompdf_lib');
 		$this->dompdf_lib->convert_html_to_pdf($html, $pdf_filename, true);
+	}
+	
+	//report using view file
+	function report_view(){
+		$pdf_filename  = 'My-report-view-file.pdf';
+		$html = $this->load->view('pdf-report-final', '', true);
+		$this->load->library('dompdf_lib');
+		$this->dompdf_lib->convert_html_to_pdf($html, $pdf_filename, true);		
 	}
 	
 }
